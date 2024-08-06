6d267a146da5e3622dcfa635b935d5e0384a0441
LeVuWan <levuwan@gmail.com>
2024-08-06 22:48:44 +0700

    video-5

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,16 @@
 import "./style.css";
 
 const MyComponent = () => {
+  // const QuangVu = "Wan Vũ";
+  // const QuangVu = [12, 6, 7];
+  const QuangVu = {
+    name: "Wan Vũ",
+    age: 22,
+  };
+
   return (
     <>
-      <div>Lê Quang Vũ</div>
+      <div>{JSON.stringify(QuangVu)}</div>
       <div className="child" style={{ borderRadius: "10px" }}>
         123
       </div>
