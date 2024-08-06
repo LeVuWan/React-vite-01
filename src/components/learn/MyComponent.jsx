e24833a1a6b94a55abc23c5e0c430d9ce65f32ff
LeVuWan <levuwan@gmail.com>
2024-08-06 22:23:52 +0700

    video-4

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,14 @@
+import "./style.css";
+
 const MyComponent = () => {
-  return <div>Lê Quang Vũ</div>;
+  return (
+    <>
+      <div>Lê Quang Vũ</div>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        123
+      </div>
+    </>
+  );
 };
 
 export default MyComponent;
