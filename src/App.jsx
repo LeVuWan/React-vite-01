7de5c5adf45d20b5322e63cc340bb984bac3b7fe
LeVuWan <levuwan@gmail.com>
2024-08-06 15:50:11 +0700

    video-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// () => {
+
+// }
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  // function myFunction() {
+  //   console.log("My function");
+  // }
+
+  const myfunction = () => {
+    console.log("Run my arrow funtion");
+  };
+
+  myfunction();
 
   return (
     <>
@@ -29,7 +43,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
