ea1645d060ba7ff9fa2bdce187941b2ef95bfdcd
LeVuWan <levuwan@gmail.com>
2024-08-07 22:04:44 +0700

    video-9

--- src/components/todos/TodoInput.jsx
+++ src/components/todos/TodoInput.jsx
@@ -3,11 +3,22 @@ import "./todo.css";
 const TodoInput = (props) => {
   console.log("Check props: " + props.addNewTodo);
   const { addNewTodo } = props;
+  const handleClick = () => {
+    alert("Click me");
+  };
+  const handleOnChange = (name) => {
+    console.log("handleOnChange: ", name);
+  };
   // addNewTodo("Vux");
   return (
     <div className="input">
-      <input type="text" />
-      <button>Add</button>
+      <input
+        type="text"
+        onChange={(event) => handleOnChange(event.target.value)}
+      />
+      <button style={{ cursor: "pointer" }} onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 };
